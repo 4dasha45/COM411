3d2916def6ae5aff9fe3d7c069734df51372c173
repl.it user <replituser@example.com>
2020-10-08 14:50:58 +0000

    ascii task

--- basics/output/basics_output_multiline_message.py
+++ basics/output/basics_output_multiline_message.py
@@ -1,3 +1,5 @@
 #display message to the standard output
-print("system failure imminent!")
-print("system reboot has been initiated...")
+#print("system failure imminent!")
+#print("system reboot has been initiated...")
+print("System Failure Imminent!\nSystem reboot has been initiated...")
+print("...rebooting sensory system \n...rebooting output motors\n...rebootinghover engine")
